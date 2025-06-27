4ebfb90289084519ea38c849ab7de1bf88fbf661
leonfirme1 <leon.firme@hotmail.com>
2025-06-27 18:55:53 -0300

    Update services.tsx

--- client/src/pages/services.tsx
+++ client/src/pages/services.tsx
@@ -9,7 +9,7 @@ import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from 
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { insertServiceFormSchema, type ServiceWithClient, type InsertService, type InsertServiceForm, type Client } from "@shared/schema";
+// import { insertServiceFormSchema, type ServiceWithClient, type InsertService, type InsertServiceForm, type Client } from "@shared/schema";
 import { apiRequest, queryClient } from "@/lib/queryClient";
 import { Plus, Edit, Trash2, Search } from "lucide-react";
 import { useToast } from "@/hooks/use-toast";
